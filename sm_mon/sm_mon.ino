34c1cea5e485febea4f88bd57b842b1a86707523
Jordan Knight <jakkaj@gmail.com>
2018-12-20 17:42:52 +1100

    Added web stuff and fixed caller

--- sm_mon/sm_mon.ino
+++ sm_mon/sm_mon.ino
@@ -1,37 +1,43 @@
 #include <elapsedMillis.h>
 #include "Statistic.h" 
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+const char* ssid = "TelstraAE6511";
+const char* password = "bpjzhvssks";
+
 elapsedMillis timeElapsed;
+elapsedMillis lastPulse;
 Statistic stats;
 
-int val = 0;
-int total = 0;
-int readings = 0;
-int currentAverage = 0;
-int reportedAverage = 0;
-float stdDev = 0;
+
+int impressionsCounted = 0;
+
+bool isCounting = false;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+ 
+    delay(1000);
+    Serial.println("Connecting..");
+ 
+  }
+  Serial.println("Connected");
 }
 
 void loop() {
   delay(10);
   // put your main code here, to run repeatedly:
-  val = analogRead(0);
+  int val = analogRead(0);
   stats.add(val);
-  //Serial.print(" Val: ");
- // Serial.print(val);
-  //Serial.print("  Average: ");
- // Serial.print(stats.average(), 4);
-  
- // Serial.print("  Std deviation: ");
-
-  //Serial.print(stats.pop_stdev(), 4);
- // Serial.println();
-  //Serial.println(val);
-  float avg = stats.average();
-  
+ 
+  float avg = stats.average();  
   
   if(val < avg - (100)){
     Serial.println(avg);
@@ -43,23 +49,75 @@ void loop() {
   
   
   
-  if(timeElapsed / 1000 > 5){
-    //stdDev = stats.pop_stdev();
-    
-    Serial.println("Clear");
-    Serial.println(avg);
-    Serial.println(val);
-    //Serial.println(stdDev);
+  if(timeElapsed / 1000 > 30){  
     stats.clear();
     timeElapsed = 0;
+    send();
   }
   
 }
 
+void send(){
+  Serial.print("Sending impressions:");
+  Serial.print(impressionsCounted);
+  Serial.println();
+
+  if(WiFi.status() != WL_CONNECTED){
+    WiFi.begin(ssid, password);
+  }
+
+  while (WiFi.status() != WL_CONNECTED) {
+ 
+    delay(1000);
+    Serial.println("Connecting..");
+ 
+  }
+  
+  Serial.println("Connected");
+  
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+ 
+    HTTPClient http;  //Declare an object of class HTTPClient
+ 
+    http.begin("http://10.0.0.38:5000/impress?time=30&imp=" + impressionsCounted);  //Specify request destination
+    int httpCode = http.GET();                                                                  //Send the request
+ 
+    if (httpCode > 0) { //Check the returning code
+ 
+      String payload = http.getString();   //Get the request response payload
+      Serial.println(payload);                     //Print the response payload
+ 
+    }
+ 
+    http.end();   //Close connection
+ 
+  }else{
+    Serial.println("Not connected");
+  }
+  impressionsCounted = 0;
+  isCounting = false;
+  
+} 
+
 void checkpoint(){
-  Serial.println(" ** Checkpoint **");
+
+  //only start counting when an impression occurrs
+  if(!isCounting){   
+    Serial.println("Beginning checkpoint session"); 
+    isCounting = true;
+    timeElapsed = 0;    
+    impressionsCounted = 0;
+    return;    
+  }
+
+  if(lastPulse < 500){
+    return;
+  }
+  
+  Serial.print("Counting:  checkpoint session"); 
+  Serial.print(impressionsCounted);
+  Serial.println(); 
+  impressionsCounted ++; 
+  lastPulse = 0; 
 }
-
-//find the average low light so we can tell if there is a high light event later.
-
 
---
+++ web/SmartMeterService/Smart.Web/Controllers/ImpressionsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Smart.Web.Controllers
+{
+    [Route("impress")]
+    public class ImpressionsController : Controller
+    {
+        public IActionResult Index(int imp, int time)
+        {
+            Console.WriteLine($"Impressions: {imp} -> time: {time}");
+            return Ok("Good");
+        }
+    }
+}
