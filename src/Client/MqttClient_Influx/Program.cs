ff5dd872a09256e74faeebef7e01e601d538736f
Jordan Knight <jakkaj@gmail.com>
2019-02-09 12:15:57 +1100

    switch main system over to dark skies

--- src/Client/MqttClient_Influx/Program.cs
+++ src/Client/MqttClient_Influx/Program.cs
@@ -15,6 +15,7 @@ using System.Net.Http;
 using System.Security.Cryptography;
 using System.Threading;
 using System.Xml.Schema;
+using DarkSky.Services;
 using InfluxDB.Collector;
 using InfluxDB.LineProtocol.Client;
 using Microsoft.Extensions.Configuration;
@@ -23,7 +24,10 @@ namespace MqttTestClient
 {
     class Program
     {
-        private const string CITY = "Balmain";
+        private static string _apiEnvVar = "DARK_SKY_API_KEY";
+        private static double _latitude = -33.86;
+        private static double _longitude = 151.18;
+
         private static double _kwh = 0;
         private static string _mqttServerAddress;
         private static string _influxServerAddress;
@@ -40,7 +44,7 @@ namespace MqttTestClient
             _influxServerAddress = config["INFLUX_SERVER_ADDRESS"];
             _powerBiServerUrl = config["POWER_BI_URL"];
 
-            _apiKey = config["OPEN_WEATHER_API_KEY"];
+            _apiKey = config["_apiEnvVar"];
 
             if (string.IsNullOrWhiteSpace(_mqttServerAddress) || string.IsNullOrWhiteSpace(_influxServerAddress)
                                                              || string.IsNullOrWhiteSpace(_powerBiServerUrl))
@@ -131,19 +135,45 @@ namespace MqttTestClient
             return builder.Build();
         }
 
+        
+
+        private static async Task<(double temp, double humid, double pressure,
+            double wind, double minToday, double maxToday,
+            double minTomorrow, double maxTomorrow)> _getWeather()
+        {
+            var weather = new DarkSkyService(_apiKey);
+
+            var forecast = await weather.GetForecast(_latitude, _longitude, new DarkSkyService.OptionalParameters
+            {
+                MeasurementUnits = "si",
+            });
+
+            var humid = forecast.Response.Currently.Humidity ?? 0;
+
+            var temp = forecast.Response.Currently.Temperature ?? 0;
+
+            var pressure = forecast.Response.Currently.Pressure ?? 0;
+
+            var wind = forecast.Response.Currently.WindSpeed ?? 0;
+
+            var minToday = forecast.Response.Daily.Data[0].TemperatureLow ?? 0;
+            var maxToday = forecast.Response.Daily.Data[0].TemperatureHigh ?? 0;
+
+            var minTomorrow = forecast.Response.Daily.Data[1].TemperatureLow ?? 0;
+            var maxTomorrow = forecast.Response.Daily.Data[1].TemperatureHigh ?? 0;
+
+            return (temp, humid, pressure, wind, minToday, maxToday, minTomorrow, maxTomorrow);
+        }
+
         static async Task _loop()
         {
             
             var influxUrl = $"http://{_influxServerAddress}:8086";
 
             await InfluxHelper.CreateDatabase(influxUrl);
+            var currentWeather = await _getWeather();
 
-            var weatherApi = new OpenWeatherMap.OpenWeatherMap(_apiKey);
-            var currentWeather = await weatherApi.GetCurrent(CITY);
-
-            var maxMax = await weatherApi.GetMaximums(CITY);
-            var minMin = await weatherApi.GetMinimums(CITY);
-
+            
             Metrics.Collector = new CollectorConfiguration()
                 .Tag.With("host", "campbellst")
                 .WriteTo.InfluxDB(influxUrl, "kwh")
@@ -156,17 +186,11 @@ namespace MqttTestClient
                 
                 Thread.Sleep(TimeSpan.FromSeconds(5));
                 
-                if(counter % 10 == 0){
-                    currentWeather = await weatherApi.GetCurrent(CITY);
+                if(counter % 180 == 0){
+                    currentWeather = await _getWeather();
                 }
 
-                if (counter % 100 == 0)
-                {
-                    maxMax = await weatherApi.GetMaximums(CITY);
-                    minMin = await weatherApi.GetMinimums(CITY);
-                }
-
-                Console.WriteLine($"Temp: {currentWeather.Main.Temp}, humidity: {currentWeather.Main.Humidity}");
+                Console.WriteLine($"Temp: {currentWeather.temp}, humidity: {currentWeather.humid}");
 
                 if(DateTime.Now.Subtract(TimeSpan.FromSeconds(60)) > _lastReading){
                     _kwh = 0;
@@ -181,22 +205,21 @@ namespace MqttTestClient
 
                 var resultDollars = ((decimal)resultDay * 28.52M) / 100M;
 
-                await PowerBIHelper.Push(_kwh, resultDollars, _powerBiServerUrl);
+                //await PowerBIHelper.Push(_kwh, resultDollars, _powerBiServerUrl);
                 Console.WriteLine("Sending");
                 //var c = new HttpClient();
                 Metrics.Write("kwh",
                     new Dictionary<string, object>
                     {
                         { "reading", _kwh },
-                        { "temp", currentWeather.Main.Temp},
-                        { "humidity", currentWeather.Main.Humidity},
-                        { "pressure", currentWeather.Main.Pressure},
-                        { "windSpeed", currentWeather.Wind.Speed},
-                        { "icon", currentWeather.WeatherWeather[0].Icon},
-                        { "min", minMin.today},
-                        { "max", maxMax.today},
-                        { "minTomorrow", minMin.tomorrow},
-                        { "maxTomorrow", maxMax.tomorrow}
+                        { "temp", currentWeather.temp},
+                        { "humidity", currentWeather.humid},
+                        { "pressure", currentWeather.pressure},
+                        { "windSpeed", currentWeather.wind},
+                        { "min", currentWeather.minToday},
+                        { "max", currentWeather.maxToday},
+                        { "minTomorrow", currentWeather.minTomorrow},
+                        { "maxTomorrow", currentWeather.maxTomorrow}
 
                     });
             }
