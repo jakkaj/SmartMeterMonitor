41f287863ed8df7db4e0b4b3319b850c27d18c89
Jordan Knight <jakkaj@gmail.com>
2019-05-29 15:40:37 +1000

    Added amber bits

---
+++ src/Client/AmberElectric/Service/AmberService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using AmberElectric.Model;
+using Newtonsoft.Json;
+
+namespace AmberElectric.Service
+{
+    public class AmberService
+    {
+        public double InPrice(AmberData data, VariablePricesAndRenewable variables)
+        {
+            var price = (data.data.staticPrices.E1.totalfixedKWHPrice + data.data.staticPrices.E1.lossFactor * data.data.variablePricesAndRenewables[0].wholesaleKWHPrice) / 1.1;
+
+            return price;
+        }
+
+        public double OutPrice(AmberData data, VariablePricesAndRenewable variables)
+        {
+            var price = (data.data.staticPrices.B1.totalfixedKWHPrice + data.data.staticPrices.B1.lossFactor * data.data.variablePricesAndRenewables[0].wholesaleKWHPrice) / 1.1;
+
+            return price;
+        }
+
+        public async Task<AmberData> Get(string postCode)
+        {
+            var data = $"{{ \"postcode\": \"{postCode}\" }}";
+
+            var url = "";
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var result = await client.PostAsync(new Uri(url), new StringContent(data));
+
+                if (!result.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+
+                var stringResult = await result.Content.ReadAsStringAsync();
+                var deSerialise = JsonConvert.DeserializeObject<AmberData>(stringResult);
+               
+                
+                var outPrice = (deSerialise.data.staticPrices.B1.totalfixedKWHPrice + deSerialise.data.staticPrices.B1.lossFactor * deSerialise.data.variablePricesAndRenewables[0].wholesaleKWHPrice) / 1.1;
+
+                return deSerialise;
+                
+                
+            }
+        }
+    }
+}
---
+++ src/Client/PowerSystemClient.Tests/AmberTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AmberElectric.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PowerSystemClient.Tests
+{
+    [TestClass]
+    public class AmberTests : TestBase
+    {
+        [TestMethod]
+        public async Task TestGetAmberData()
+        {
+            var amber = new AmberService();
+
+            var predict = await amber.Get("2047");
+
+            Assert.IsNotNull(predict);
+
+            var variable = predict.data.variablePricesAndRenewables.OrderBy(_ => _.createdAt)
+                .Last(_ => _.periodType == "ACTUAL");
+
+            var inPrice = amber.InPrice(predict, variable);
+
+            var outPrice = amber.OutPrice(predict, variable);
+        }
+
+    }
+}
