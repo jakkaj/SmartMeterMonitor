4943013143794724910e0dbf988b42c16aac5c58
Jordan Knight <jakkaj@gmail.com>
2019-08-09 11:23:56 +1000

    updated to return json

--- src/Modbus/jk.py
+++ src/Modbus/jk.py
@@ -6,15 +6,30 @@ import time
 
 d = client.SunSpecClientDevice(client.TCP, 2, ipaddr="192.168.0.201")
 
-print (d.models)
-print d.inverter.points
+#print (d.models)
+#print d.inverter.points
 
 while True:
 
+
+    
+    
     d.inverter.read()
-    export = json.dumps(d.inverter)
-    print export
-    print d.inverter["W"]
+    dict = {}
+    for (key, value) in d.inverter.model.__dict__.items():
+        #print(key)
+        if(key == "points"):
+            
+            for(k, v) in value.items():
+                #print(k)
+                #print(v.value_base)
+                dict[k] = d.inverter[k]
+            
+        #print(str(value))
+    dumped = json.dumps(dict)
+    print(dumped)
+    #print export
+    #print d.inverter["W"]
     print d.inverter["PhVphA"]
-    print d.inverter["VAr"]
+    #print d.inverter["VAr"]
     time.sleep(5)
--- src/Modbus/server.py
+++ src/Modbus/server.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import request
+from flask import Response
+import json
 app = Flask(__name__)
 
 import sunspec.core.client as client
@@ -8,11 +10,23 @@ import sunspec.core.client as client
 @app.route('/inverter')
 def hello_world():   
     ip = request.args.get('ip')
-    field = request.args.get('field')
+    
     d = client.SunSpecClientDevice(client.TCP, 2, ipaddr=ip)
     d.inverter.read()
-    return  str(d.inverter[field])
+    dict = {}
+    for (key, value) in d.inverter.model.__dict__.items():
+        #print(key)
+        if(key == "points"):
+            
+            for(k, v) in value.items():
+                #print(k)
+                #print(v.value_base)
+                dict[k] = d.inverter[k]
+            
+        #print(str(value))
+    dumped = json.dumps(dict)
+    #print(dumped)
+    return Response(dumped, mimetype="application/json")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-    
+    app.run(debug=True, host='0.0.0.0')    
