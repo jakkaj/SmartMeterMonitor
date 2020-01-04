eb98822a3a647033c0baf1fce7127fbe6c654f5e
Jordan Knight <jakkaj@gmail.com>
2020-01-05 10:34:54 +1100

    Fix: TestMode missing

--- src/Client/EnergyHost.Model/Settings/EnergyHostSettings.cs
+++ src/Client/EnergyHost.Model/Settings/EnergyHostSettings.cs
@@ -24,5 +24,7 @@ namespace EnergyHost.Model.Settings
         public string ALEXA_NOTIFICATION_KEY { get; set; } //https://www.thomptronics.com/about/notify-me
         public string PUSHOVER_USER { get; set; } //https://pushover.net/api
         public string PUSHOVER_TOKEN { get; set; }
+
+        public bool TestMode = false;
     }
 }
