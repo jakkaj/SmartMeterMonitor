ef6a49c15c2e03ef42f8dc806b7e2efb5a7a1535
Jordan Knight <jakkaj@gmail.com>
2020-01-18 14:30:00 +1100

    Fix types

--- src/Client/EnergyHost.Services/Services/DataLoggerService.cs
+++ src/Client/EnergyHost.Services/Services/DataLoggerService.cs
@@ -312,11 +312,11 @@ namespace EnergyHost.Services.Services
                     SolarOutput = Convert.ToDouble(abbModbus.W) / 1000;
                     if (abbModbus?.energyDetails != null)
                     {
-                        SolarToday = Convert.ToDouble(abbModbus.energyDetails.meters.First(_ => _.type == "Production").values[0].value / 1000);
-                        Consumption = Convert.ToDouble(abbModbus.energyDetails.meters.First(_ => _.type == "Consumption").values[0].value / 1000);
-                        Purchased = Convert.ToDouble(abbModbus.energyDetails.meters.First(_ => _.type == "Purchased").values[0].value / 1000);
-                        SelfConsumption = Convert.ToDouble(abbModbus.energyDetails.meters.First(_ => _.type == "SelfConsumption").values[0].value / 1000);
-                        FeedIn = Convert.ToDouble(abbModbus.energyDetails.meters.First(_ => _.type == "FeedIn").values[0].value / 1000);
+                        SolarToday = Convert.ToDouble(abbModbus.energyDetails.meters.First(_ => _.type == "Production").values[0].value) / 1000;
+                        Consumption = Convert.ToDouble(abbModbus.energyDetails.meters.First(_ => _.type == "Consumption").values[0].value) / 1000;
+                        Purchased = Convert.ToDouble(abbModbus.energyDetails.meters.First(_ => _.type == "Purchased").values[0].value) / 1000;
+                        SelfConsumption = Convert.ToDouble(abbModbus.energyDetails.meters.First(_ => _.type == "SelfConsumption").values[0].value) / 1000;
+                        FeedIn = Convert.ToDouble(abbModbus.energyDetails.meters.First(_ => _.type == "FeedIn").values[0].value) / 1000;
 
                     }
 
