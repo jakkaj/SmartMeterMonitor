a69b18ee30b5debb7304339655a43262faa0cae6
Jordan Knight <jakkaj@gmail.com>
2020-08-01 11:13:07 +1000

    Updated to support cron job

--- src/Util/backup.sh
+++ src/Util/backup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+
+. /home/jordan/.bashrc
 container=$(docker ps | grep "influxdb" | awk '{ print $1 }')
 date=$(date -Is)
 dir=/data/influx2/$date
 mkdir $dir
 command="influxd backup -portable -host 127.0.0.1:8088 ./var/lib/influxdb/$date"
-docker exec -it $container $command
+/usr/bin/docker exec $container $command
 zip -r /home/jordan/backup/influx/$date.zip $dir
 rm -r $dir
 
