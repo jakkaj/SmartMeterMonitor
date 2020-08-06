846ad26ba0c1a38e197848f3706aab9b941ff7ef
Jordan Knight <jakkaj@gmail.com>
2020-08-06 11:04:32 +1000

    updates for pi

--- src/Util/backup.sh
+++ src/Util/backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #cron 0 0 */2 * * /home/jordan/GitHub/SmartMeterMonitor3/src/Util/backup.sh > /home/jordan/cron.log 2>&1
 
-. /home/jordan/.bashrc
+. /home/pi/.bashrc
 container=$(docker ps | grep "influxdb" | awk '{ print $1 }')
 date=$(date -Is)
 dir=/data/influx2/$date
 mkdir $dir
 command="influxd backup -portable -host 127.0.0.1:8088 ./var/lib/influxdb/$date"
 /usr/bin/docker exec $container $command
-zip -r /home/jordan/backup/influx/$date.zip $dir
+zip -r /home/pi/backup/influx/$date.zip $dir
 rm -r $dir
 
