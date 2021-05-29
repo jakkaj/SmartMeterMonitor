245e6a54ab8fb4862c4ec24daeb5b96f4e143d42
Jordan Knight <jakkaj@gmail.com>
2021-05-30 08:28:29 +1000

    Move to tokens only

--- src/Tesla/server.py
+++ src/Tesla/server.py
@@ -5,6 +5,11 @@ import json
 import time
 from datetime import datetime
 from datetime import timedelta 
+
+import json
+from datetime import datetime
+
+
 app = Flask(__name__)
 
 from dotenv import load_dotenv
@@ -27,12 +32,26 @@ GW_PASSWORD=os.getenv("GW_PASSWORD")
 GW_IP=os.getenv("GW_IP")
 TESLA_USER=os.getenv("TESLA_USER")
 TESLA_PASSWORD=os.getenv("TESLA_PASSWORD")
+START_TOKEN=os.getenv("START_TOKEN")
 
 loop = asyncio.get_event_loop()
 
+async def save_token(token):
+    open("token_file.json", "w").write(token)
+
+async def load_token():
+    try:
+        with open('token_file.json', "r") as f:
+            return f.read()        
+    except IOError:
+        await save_token(START_TOKEN)
+        return await load_token()    
+    
+
 
 async def getreserve():
-    client = TeslaApiClient(TESLA_USER, TESLA_PASSWORD) 
+    token = await load_token()
+    client = TeslaApiClient(token=token, on_new_token=save_token) 
     energy_sites = await client.list_energy_sites()
     assert(len(energy_sites)==1)   
     reserve = await energy_sites[0].get_backup_reserve_percent()    
@@ -42,7 +61,8 @@ async def getreserve():
 
 
 async def setreserve(percent):
-    client = TeslaApiClient(TESLA_USER, TESLA_PASSWORD)        
+    token = await load_token()
+    client = TeslaApiClient(token=token, on_new_token=save_token)     
     energy_sites = await client.list_energy_sites()    
     assert(len(energy_sites)==1)
     print("Increment backup reserve percent")
