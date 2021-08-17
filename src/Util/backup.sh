46aabd1ae770e03f167205f4ba18093f58cf1cab
Jordan Knight <jakkaj@gmail.com>
2021-08-17 21:02:17 +1000

    Fix up for port back from pi

--- src/Util/backup.sh
+++ src/Util/backup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #cron 0 0 */2 * * /home/jordan/GitHub/SmartMeterMonitor3/src/Util/backup.sh > /home/jordan/cron.log 2>&1
 
-. /home/pi/.bashrc
+. /home/jak/.bashrc
 container=$(docker ps | grep "influxdb" | awk '{ print $1 }')
 date=$(date -Is)
-dir=/data/influx2/$date
+dir=/data/influx1/$date
 mkdir $dir
 command="influxd backup -portable -host 127.0.0.1:8088 ./var/lib/influxdb/$date"
 /usr/bin/docker exec $container $command
 cp /data/grafana/grafana.db $dir/grafana.db
-zip -r /home/pi/backup/influx/$date.zip $dir
-rclone copy /home/pi/backup/influx/$date.zip onedrive:backup/house
-rm /home/pi/backup/influx/$date.zip
+zip -r /home/jak/backup/influx/$date.zip $dir
+rclone copy /home/jak/backup/influx/$date.zip onedrive:backup/house
+rm /home/jak/backup/influx/$date.zip
 rm -r $dir  
